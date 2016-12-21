b6f1a5293baa40a7f523929fcfcb15bb4570b8c4
drorn <drorn@xpapps.co.il>
2016-12-21 11:02:31 +0200

    no message

---
+++ EX3/LINUX_COMMAND_LINE/FOLDER_3_src/ClassChecker.java
@@ -0,0 +1,22 @@
+
+public class ClassChecker {
+	public class Class{
+		String name;
+		Class parent;
+		LinkedList<> classProps;
+		
+		boolean isSuperChild(Class superParent){
+			//TODO
+		}
+	}
+	
+	public ClassChecker(){
+		
+	}
+	
+	
+	
+	
+	
+	
+}
