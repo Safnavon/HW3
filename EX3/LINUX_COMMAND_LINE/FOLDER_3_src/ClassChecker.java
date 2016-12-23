0611f755aa35e040375fd090076cad121d885aeb
drorn <drorn@xpapps.co.il>
2016-12-24 00:41:36 +0200

    no message

--- EX3/LINUX_COMMAND_LINE/FOLDER_3_src/ClassChecker.java
+++ EX3/LINUX_COMMAND_LINE/FOLDER_3_src/ClassChecker.java
@@ -26,10 +26,28 @@ public static class ClassChecker {
 			return myRest.first == null;//same size of data structures
 		}
 	}
+	public class Field {
+		AST_TYPE type;
+		String name;
+		public Field(AST_TYPE type, String name){
+			this.type = type;
+			this.name = name;
+		}
+
+		public boolean equals(Object other){
+			if(! other instanceof Field){
+				return false;
+			}
+			Field _other = (Field) other;
+			return 	this.type.equals(_other.type) &&
+							this.name.equals(_other.name);
+		}
+	}
 	public class Class {
 		public String name;
 		public Class parent;
 		public LinkedList<Function> funcs = new LinkedList<Function>();
+		public LinkedList<Field> fields = new LinkedList<Field>();
 
 		public Class(AST_CLASS_DECLARE cDec, Class parent){
 			this.name = cDec.name;
@@ -54,17 +72,20 @@ public static class ClassChecker {
 			}
 			return f.isSameArgsTypes(argTypes);
 		}
+
+		void addFields(AST_FIELD fs){
+			for(int i = 0; i< fs.names.length; i++){
+				this.fields.add(new Field(fs.type, fs.names[i]));
+			}
+		}
 	}
 
 	private HashMap<String, Class> map = new HashMap<String, Class>();
-	public ClassChecker(){
-
-	}
 
 	/*public Class get(String name){
 		return this.map.get(name);
 	}*/
-	public void newClass(String name, AST_CLASS_DECLARE classDec){
+	public static void newClass(String name, AST_CLASS_DECLARE classDec){
 		Class parent = null;
 		if(classDec.extend != null){
 			parent = this.map.get(classDec.extend);
@@ -73,7 +94,7 @@ public static class ClassChecker {
 		this.map.put(name, c);
 	}
 
-	public void addFunction(String className, AST_METHOD_DECLARE func) throws Exception{
+	public static void addFunction(String className, AST_METHOD_DECLARE func) throws Exception{
 		Class c = this.map.get(className);
 		if(c == null){
 			throw new Exception("Cant find class " + className);
