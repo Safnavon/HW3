bd1885571466f29a71a6364a1bb4c2cffebfd587
drorn <drorn@xpapps.co.il>
2016-12-27 21:30:48 +0200

    no message

--- EX3/LINUX_COMMAND_LINE/FOLDER_3_src/AST/AST_PROGRAM.java
+++ EX3/LINUX_COMMAND_LINE/FOLDER_3_src/AST/AST_PROGRAM.java
@@ -1,20 +1,24 @@
-package AST; import src.ClassChecker;
+package AST;
+import src.ClassChecker;
 import src.SymbolTable;
 
 public class AST_PROGRAM extends AST_Node
 {
 	public AST_CLASS_DECLARE first;
 	public AST_PROGRAM rest;
-	
+
 	public AST_PROGRAM(AST_CLASS_DECLARE first, AST_PROGRAM	 rest){
 		this.first = first;
 		this.rest = rest;
 	}
-	
+
 	@Override
 	public AST_TYPE isValid() throws Exception {
 		first.isValid();
 		if(rest!= null) rest.isValid();
-		return null;
+		List<AST_TYPE_ARRAY> args = new ArrayList<AST_TYPE_ARRAY>();
+		args.add(new AST_TYPE_ARRAY(new AST_TYPE_TERM(TYPES.STRING)));
+		AST_TYPE_CLASS mainClass = new AST_TYPE_CLASS("Main");
+		return ClassChecker.isValidMethod(mainClass, "main", args);
 	}
 }
