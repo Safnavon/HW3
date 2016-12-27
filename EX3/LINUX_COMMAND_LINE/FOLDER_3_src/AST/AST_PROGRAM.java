6628ca6195a722e4677a6e96d33746afbf5f1960
Asaf Navon <asafn@apricode.co.il>
2016-12-27 22:16:03 +0200

    no message

--- EX3/LINUX_COMMAND_LINE/FOLDER_3_src/AST/AST_PROGRAM.java
+++ EX3/LINUX_COMMAND_LINE/FOLDER_3_src/AST/AST_PROGRAM.java
@@ -1,4 +1,7 @@
 package AST;
+import java.util.ArrayList;
+import java.util.List;
+
 import src.ClassChecker;
 import src.SymbolTable;
 
@@ -16,7 +19,7 @@ public class AST_PROGRAM extends AST_Node
 	public AST_TYPE isValid() throws Exception {
 		first.isValid();
 		if(rest!= null) rest.isValid();
-		List<AST_TYPE_ARRAY> args = new ArrayList<AST_TYPE_ARRAY>();
+		List<AST_TYPE> args = new ArrayList<AST_TYPE>();
 		args.add(new AST_TYPE_ARRAY(new AST_TYPE_TERM(TYPES.STRING)));
 		AST_TYPE_CLASS mainClass = new AST_TYPE_CLASS("Main");
 		AST_TYPE mainRetType = ClassChecker.isValidMethod(mainClass, "main", args);
