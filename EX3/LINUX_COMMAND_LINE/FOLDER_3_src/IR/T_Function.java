e83c937265476803f7e943187d939aa4f79910ed
Asaf Navon <asafn@apricode.co.il>
2017-01-14 17:50:27 +0200

    no message

--- EX3/LINUX_COMMAND_LINE/FOLDER_3_src/IR/T_Function.java
+++ EX3/LINUX_COMMAND_LINE/FOLDER_3_src/IR/T_Function.java
@@ -1,5 +1,13 @@
 package IR;
 
 public class T_Function implements T_Exp{
-
+	public T_Exp prologue, epilogue, body;
+	public String name;
+	
+	public T_Function(T_Exp prologue, T_Exp epilogue, T_Exp body, String name){
+		this.prologue=prologue;
+		this.epilogue=epilogue;
+		this.body=body;
+		this.name=name;
+	}
 }
--- EX3/LINUX_COMMAND_LINE/FOLDER_3_src/IR/T_Move.java
+++ EX3/LINUX_COMMAND_LINE/FOLDER_3_src/IR/T_Move.java
@@ -1,5 +1,10 @@
 package IR;
 
 public class T_Move implements T_Exp {
-
+	T_Exp src,dest;
+	
+	public T_Move (T_Exp src, T_Exp dest){
+		this.src=src;
+		this.dest=dest;
+	}
 }
