191292fc4dbf5359d3f0fb27b8616a7aef6751e1
drorn <dexenerator@gmail.com>
2017-01-31 19:17:10 +0200

    fixes to ensure one main

--- EX3/LINUX_COMMAND_LINE/FOLDER_3_src/AST/AST_PROGRAM.java
+++ EX3/LINUX_COMMAND_LINE/FOLDER_3_src/AST/AST_PROGRAM.java
@@ -1,4 +1,5 @@
 package AST;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -6,30 +7,28 @@ import src.ClassChecker;
 import src.IR_TYPE_WRAPPER;
 import src.SymbolTable;
 
-public class AST_PROGRAM extends AST_Node
-{
-	public AST_CLASS_DECLARE first;
-	public AST_PROGRAM rest;
+public class AST_PROGRAM extends AST_Node {
+    public AST_CLASS_DECLARE first;
+    public AST_PROGRAM rest;
 
-	public AST_PROGRAM(AST_CLASS_DECLARE first, AST_PROGRAM	 rest){
-		this.first = first;
-		this.rest = rest;
-	}
+    public AST_PROGRAM(AST_CLASS_DECLARE first, AST_PROGRAM rest) {
+        this.first = first;
+        this.rest = rest;
+    }
 
-	public IR_TYPE_WRAPPER isValid() throws Exception {
-		first.isValid();
-		if(rest!= null) rest.isValid();
-		return new IR_TYPE_WRAPPER(null,null);//TODO
-	}
+    public IR_TYPE_WRAPPER isValid() throws Exception {
+        IR_TYPE_WRAPPER firstWrapper = first.isValid();
+        IR_TYPE_WRAPPER restWrapper = rest != null ? rest.isValid() : null;
+        return new IR_TYPE_WRAPPER(null, null);//TODO
+    }
 
-	/**
-	 * avoids multiple calls to "ensureOneMain"
-	 *
-	 */
-	public void isValidProgram() throws Exception {
-		IR_TYPE_WRAPPER wrapper = this.isValid();
-		ClassChecker.ensureOneMain();//throws if bad
+    /**
+     * avoids multiple calls to "ensureOneMain"
+     */
+    public void isValidProgram() throws Exception {
+        IR_TYPE_WRAPPER wrapper = this.isValid();
+        ClassChecker.ensureOneMain();//throws if bad
         //TODO use ensure main to get label of program entry point
-	}
+    }
 
 }
