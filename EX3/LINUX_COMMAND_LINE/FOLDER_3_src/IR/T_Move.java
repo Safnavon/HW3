78cc522cbc8c57a726e81153f970390b658a7717
drorn <dexenerator@gmail.com>
2017-03-04 18:30:33 +0200

    new instance

--- EX3/LINUX_COMMAND_LINE/FOLDER_3_src/IR/T_Move.java
+++ EX3/LINUX_COMMAND_LINE/FOLDER_3_src/IR/T_Move.java
@@ -10,8 +10,9 @@ public class T_Move implements T_Exp {
 
 	@Override
 	public T_Temp gen() {
-		//TODO if src is T_Label, use "la $t0, var1" (assert dst is Temp)
-		//TODO else if move.dst is a T_Mem, use "sw". if move.dst is a T_Temp, use "addi dst,src,0"
+		//TODO if src is T_Label, use "la $t0, var1" (assert dst is Temp or Mem)
+        //TODO  if src is T_Malloc, gen malloc (returns T_Temp) and use result as temp
+		//TODO  if move.dst is a T_Mem, use "sw". if move.dst is a T_Temp, use "addi dst,src,0"
 
 		throw new Error("unimplemented");
 	}
