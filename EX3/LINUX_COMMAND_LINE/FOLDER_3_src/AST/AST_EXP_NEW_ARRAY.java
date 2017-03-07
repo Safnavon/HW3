aaf223dd8eeaca3ae63b53b03503931255f87e95
Asaf Navon <asafn@apricode.co.il>
2017-03-07 17:29:48 +0200

    .

--- EX3/LINUX_COMMAND_LINE/FOLDER_3_src/AST/AST_EXP_NEW_ARRAY.java
+++ EX3/LINUX_COMMAND_LINE/FOLDER_3_src/AST/AST_EXP_NEW_ARRAY.java
@@ -31,45 +31,43 @@ public class AST_EXP_NEW_ARRAY extends AST_EXP {
 
     public T_Exp buildIr(){
 
-        // Allocating the array:
-        ArrayList<T_Exp> expList = new ArrayList<>();
+
+        ArrayList<T_Exp> resExpList = new ArrayList<>();
         T_Temp locationTemp =new T_Temp();
         T_Temp sizeTemp = (T_Temp)size.buildIr();
-        ArrayList<T_Exp> expList2 = new ArrayList<>();
-        //expList2.add(sizeTemp);
-        T_Binop bin=new T_Binop(BINOPS.PLUS, sizeTemp, new T_Const(1));
-        expList2.add( new T_Binop(BINOPS.TIMES,bin,new T_Const(4)));
-        T_ESeq size = new T_ESeq(expList2);
-        T_Move node = new T_Move(locationTemp, new T_Malloc(size));
-        expList.add(node);
+        ArrayList<T_Exp> calcSizeExps = new ArrayList<>();
 
-        // Putting the size of the array in the first spot.
-        node = new T_Move(new T_Mem(new T_Binop(BINOPS.PLUS,locationTemp,new T_Const(0))), sizeTemp);
-        expList.add(node);
+        T_Binop op=new T_Binop(BINOPS.PLUS, sizeTemp, new T_Const(1));
+        calcSizeExps.add( new T_Binop(BINOPS.TIMES,op,new T_Const(4)));
+        T_ESeq size = new T_ESeq(calcSizeExps);
+        T_Move resExp = new T_Move(locationTemp, new T_Malloc(size));
+        resExpList.add(resExp);
 
-        // initializing the array:
+        resExp = new T_Move(new T_Mem(new T_Binop(BINOPS.PLUS,locationTemp,new T_Const(0))), sizeTemp);
+        resExpList.add(resExp);
+
         T_Temp count = new T_Temp();
-        node = new T_Move(count, new T_Const(1));
-        expList.add(node);
+        resExp = new T_Move(count, new T_Const(1));
+        resExpList.add(resExp);
         T_Label loop = new T_Label("Loop", true);
         T_Label exit = new T_Label("Exit", true);
         T_CJump cJumpExit = new T_CJump(new T_Relop(RELOPS.GT, count, sizeTemp), exit);
         T_JumpLabel jumpLoop = new T_JumpLabel(loop);
-        expList.add(loop);
-        expList.add(cJumpExit);
+        resExpList.add(loop);
+        resExpList.add(cJumpExit);
 
         T_Binop address = new T_Binop(BINOPS.PLUS, locationTemp, new T_Binop(BINOPS.PLUS, count, new T_Const(4)));
-        node = new T_Move(new T_Mem(address), new T_Const(0));
-        expList.add(node);
+        resExp = new T_Move(new T_Mem(address), new T_Const(0));
+        resExpList.add(resExp);
 
-        node = new T_Move(count, new T_Binop(BINOPS.PLUS, count, new T_Const(1)));
-        expList.add(node);
+        resExp = new T_Move(count, new T_Binop(BINOPS.PLUS, count, new T_Const(1)));
+        resExpList.add(resExp);
 
-        expList.add(jumpLoop);
-        expList.add(exit);
+        resExpList.add(jumpLoop);
+        resExpList.add(exit);
 
-        expList.add(locationTemp);
-        return new T_ESeq(expList);
+        resExpList.add(locationTemp);
+        return new T_ESeq(resExpList);
 
     }
 
