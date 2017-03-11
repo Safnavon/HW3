b4ed20f3f32c69cbc9edb88a627caa40f937eab9
drorn <drorn@xpapps.co.il>
2017-03-11 15:33:42 +0200

    syscall fix

--- EX3/LINUX_COMMAND_LINE/FOLDER_3_src/IR/T_Malloc.java
+++ EX3/LINUX_COMMAND_LINE/FOLDER_3_src/IR/T_Malloc.java
@@ -24,8 +24,8 @@ public class T_Malloc implements T_Exp {
         T_Temp size = this.size.gen();
         //TODO
         CGen.append(String.format(
-                "\tli\t$v0, 9%n"+
                 "\taddi\t$a0, %1$s, 0%n"+
+                "\tli\t$v0,9%n"+
                 "\t\tsyscall%n"+
                 "\taddi\t%2$s, $v0, 0%n",
                 size, res
