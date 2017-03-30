cf8acab1fb2a7f8f562a25ea369240d8dcdfee98
drorn <drorn@xpapps.co.il>
2017-03-30 20:18:03 +0300

    if stmt fix

--- EX3/LINUX_COMMAND_LINE/FOLDER_3_src/AST/AST_STMT_IF.java
+++ EX3/LINUX_COMMAND_LINE/FOLDER_3_src/AST/AST_STMT_IF.java
@@ -1,30 +1,31 @@
-package AST; import IR.*;
+package AST;
+
+import IR.*;
 import src.ClassChecker;
 import src.IR_TYPE_WRAPPER;
 import src.SymbolTable;
 
-public class AST_STMT_IF extends AST_STMT
-{
-	public AST_EXP cond;
-	public AST_STMT body;
+public class AST_STMT_IF extends AST_STMT {
+    public AST_EXP cond;
+    public AST_STMT body;
 
-	/*******************/
-	/*  CONSTRUCTOR(S) */
-	/*******************/
-	public AST_STMT_IF(AST_EXP cond,AST_STMT body)
-	{
-		this.cond = cond;
-		this.body = body;
-	}
-	
-	
-	public IR_TYPE_WRAPPER isValid(AST_TYPE expectedReturnValue) throws Exception {
-		IR_TYPE_WRAPPER condWrapper = cond.isValid();
-		if (! condWrapper.type.equals(new AST_TYPE_TERM(TYPES.INT))) {
-			throw new Exception("if statement condition must be an integer");
-		}
-		SymbolTable.openScope();
-		body.isValid(expectedReturnValue);
+    /*******************/
+    /*  CONSTRUCTOR(S) */
+
+    /*******************/
+    public AST_STMT_IF(AST_EXP cond, AST_STMT body) {
+        this.cond = cond;
+        this.body = body;
+    }
+
+
+    public IR_TYPE_WRAPPER isValid(AST_TYPE expectedReturnValue) throws Exception {
+        IR_TYPE_WRAPPER condWrapper = cond.isValid();
+        if (!condWrapper.type.equals(new AST_TYPE_TERM(TYPES.INT))) {
+            throw new Exception("if statement condition must be an integer");
+        }
+        SymbolTable.openScope();
+        body.isValid(expectedReturnValue);
         SymbolTable.closeScope();
 
 //		T_CJump irJump;//TODO
@@ -34,15 +35,15 @@ public class AST_STMT_IF extends AST_STMT
 //
 //		return new IR_TYPE_WRAPPER(null,s1); //TODO
         return null;
-	}
+    }
 
-	public T_Exp buildIr(){
+    public T_Exp buildIr() {
 
-		T_Exp expVal = cond.buildIr();
-		T_Label label = new T_Label("EndIf",true);
-		T_Exp bodyIr = body.buildIr();
-		T_CJump jumpIr = new T_CJump(expVal, label);
-		return new T_Seq(jumpIr,new T_Seq(bodyIr,label));
+        T_Exp expVal = cond.buildIr();
+        T_Label label = new T_Label("EndIf", true);
+        T_Exp bodyIr = body.buildIr();
+        T_CJump jumpIr = new T_CJump(new T_Relop(RELOPS.EQUAL, expVal, new T_Const(0)), label);
+        return new T_Seq(jumpIr, new T_Seq(bodyIr, label));
 
-	}
+    }
 }
